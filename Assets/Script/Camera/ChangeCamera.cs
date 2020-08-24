7aac894d8b7ab1f9f25ce1e034350611b24c107a
Giovanni Brunetti <gb1609@outlook.com>
2020-08-25 01:46:35 +0200

    Add Middle Camera

---
+++ Assets/Script/Camera/ChangeCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public GameObject mini;
+    public GameObject middle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mini.SetActive(true);
+        middle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (mini.activeSelf) mini.SetActive(false);
+            if (!middle.activeSelf) middle.SetActive(true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            if (!mini.activeSelf) mini.SetActive(true);
+            if (middle.activeSelf) middle.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/Camera/PlayerCameraManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerCameraManager : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    public GameObject crosshair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crosshair = GameObject.FindGameObjectWithTag("CrossHair");
+        crosshair.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            crosshair.SetActive(!crosshair.activeSelf);
+            cameraTransform.position = (crosshair.activeSelf)
+                ? cameraTransform.position += cameraTransform.forward * 3
+                : cameraTransform.position -= cameraTransform.forward * 3;
+        }
+    }
+}
