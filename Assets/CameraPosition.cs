c01d296221b90c75479c7af673af8bd1e8d193b1
Giovanni Brunetti <gb1609@outlook.com>
2020-08-25 00:59:17 +0200

    Fix MiniMap

---
+++ Assets/CameraPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    public RectTransform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+}
--- Assets/Script/CameraMap/MiniMapManager.cs
+++ Assets/Script/CameraMap/MiniMapManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class MiniMapManager : MonoBehaviour
 {
     public Transform playerTransform;
 
@@ -8,17 +8,19 @@ public class FollowPlayer : MonoBehaviour
 
     private float minimumX = -360F;
     private float maximumX = 360F;
+    public int distances;
 
     void Start()
     {
-        transform.position = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y+ distances, playerTransform
+        .position.z);
         transform.rotation = Quaternion.Euler(new Vector3(90, playerTransform.rotation.y, 0));
     }
 
     void LateUpdate()
     {
         transform.position = new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z);
-        float rotationX= Input.GetAxis("Mouse X") * sensitivy;
+        float rotationX = Input.GetAxis("Mouse X") * sensitivy;
         rotationX = ClampAngleHorizontal(rotationX, minimumX, maximumX);
         Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.back);
         transform.localRotation = transform.rotation * xQuaternion;
